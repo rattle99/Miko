a1a596ed3fc7bc4774f0ee4442012a6746d5602a
rattle99 <24495512+rattle99@users.noreply.github.com>
2019-06-28 00:19:54 +0530

    Allow fetching all CMC coins
    - Add BTC
    - Allow fetching all coins from CMC with full ticker name

--- pricebot.py
+++ pricebot.py
@@ -55,4 +55,40 @@ async def bhd(ctx):
 
     await ctx.send(embed = embed)
 
+@client.command()
+async def btc(ctx):
+    embed = discord.Embed(
+        colour = discord.Colour.orange()
+    )
+
+    coin = getcoin.Coin('bitcoin')
+    
+    embed.set_footer(text = 'Prices from CoinMarketCap', icon_url = 'https://i.imgur.com/N04AybW.jpg')
+    embed.set_author(name = 'Bitcoin', icon_url = 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png')
+    embed.add_field(name = 'Price', value = coin.price, inline = True)
+    embed.add_field(name = 'Rank', value = coin.rank, inline = True)
+    embed.add_field(name = 'Swing', value = coin.swing, inline = False)
+    embed.add_field(name = 'Market Cap', value = coin.market_cap, inline = True)
+    embed.add_field(name = '24H Volume', value = coin.volume, inline = True)
+
+    await ctx.send(embed = embed)
+
+@client.command()
+async def get(ctx, name):
+    embed = discord.Embed(
+        colour = discord.Colour.light_grey()
+    )
+
+    coin = getcoin.Coin(name)
+    
+    embed.set_footer(text = 'Prices from CoinMarketCap', icon_url = 'https://i.imgur.com/N04AybW.jpg')
+    embed.set_author(name = name.capitalize())
+    embed.add_field(name = 'Price', value = coin.price, inline = True)
+    embed.add_field(name = 'Rank', value = coin.rank, inline = True)
+    embed.add_field(name = 'Swing', value = coin.swing, inline = False)
+    embed.add_field(name = 'Market Cap', value = coin.market_cap, inline = True)
+    embed.add_field(name = '24H Volume', value = coin.volume, inline = True)
+
+    await ctx.send(embed = embed)
+
 client.run(TOKEN)    
