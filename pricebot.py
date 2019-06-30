b7f92f98bfd058f74d158ff98116c929519c05c2
rattle99 <24495512+rattle99@users.noreply.github.com>
2019-06-30 21:20:15 +0530

    Refactor code
    - Add method to generate embeds.
    - Add cool down timer for bot command.
      - Add error handler for the same.

--- pricebot.py
+++ pricebot.py
@@ -6,7 +6,7 @@ import getcoin
 user_token = open("token", "r")
 TOKEN = user_token.read().rstrip()
 user_token.close()
-print(TOKEN)
+print('Bot token :', TOKEN)
 
 
 client = commands.Bot(command_prefix = '!')
@@ -14,69 +14,15 @@ client = commands.Bot(command_prefix = '!')
 @client.event
 async def on_ready():
     print('Bot is ready!')
+    print('Logged in as {0.user}'.format(client))
 
-@client.command()
+@client.command(aliases=['Ping'])
 async def ping(ctx):
     await ctx.send('Pong!')
 
-@client.command()
-async def burst(ctx):
+def makeEmbed(name):
     embed = discord.Embed(
-        colour = discord.Colour.blue()
-    )
-
-    coin = getcoin.Coin('burst')
-    
-    embed.set_footer(text = 'Prices from CoinMarketCap', icon_url = 'https://i.imgur.com/N04AybW.jpg')
-    embed.set_author(name = 'BURST', icon_url = 'https://i.imgur.com/ipP9Bu3.png')
-    embed.add_field(name = 'Price', value = coin.price, inline = True)
-    embed.add_field(name = 'Rank', value = coin.rank, inline = True)
-    embed.add_field(name = 'Swing', value = coin.swing, inline = False)
-    embed.add_field(name = 'Market Cap', value = coin.market_cap, inline = True)
-    embed.add_field(name = '24H Volume', value = coin.volume, inline = True)
-
-    await ctx.send(embed = embed)
-
-@client.command()
-async def bhd(ctx):
-    embed = discord.Embed(
-        colour = discord.Colour.orange()
-    )
-
-    coin = getcoin.Coin('bitcoinhd')
-    
-    embed.set_footer(text = 'Prices from CoinMarketCap', icon_url = 'https://i.imgur.com/N04AybW.jpg')
-    embed.set_author(name = 'BitcoinHD', icon_url = 'https://s2.coinmarketcap.com/static/img/coins/64x64/3966.png')
-    embed.add_field(name = 'Price', value = coin.price, inline = True)
-    embed.add_field(name = 'Rank', value = coin.rank, inline = True)
-    embed.add_field(name = 'Swing', value = coin.swing, inline = False)
-    embed.add_field(name = 'Market Cap', value = coin.market_cap, inline = True)
-    embed.add_field(name = '24H Volume', value = coin.volume, inline = True)
-
-    await ctx.send(embed = embed)
-
-@client.command()
-async def btc(ctx):
-    embed = discord.Embed(
-        colour = discord.Colour.orange()
-    )
-
-    coin = getcoin.Coin('bitcoin')
-    
-    embed.set_footer(text = 'Prices from CoinMarketCap', icon_url = 'https://i.imgur.com/N04AybW.jpg')
-    embed.set_author(name = 'Bitcoin', icon_url = 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png')
-    embed.add_field(name = 'Price', value = coin.price, inline = True)
-    embed.add_field(name = 'Rank', value = coin.rank, inline = True)
-    embed.add_field(name = 'Swing', value = coin.swing, inline = False)
-    embed.add_field(name = 'Market Cap', value = coin.market_cap, inline = True)
-    embed.add_field(name = '24H Volume', value = coin.volume, inline = True)
-
-    await ctx.send(embed = embed)
-
-@client.command()
-async def get(ctx, name):
-    embed = discord.Embed(
-        colour = discord.Colour.light_grey()
+        colour = discord.Colour.from_rgb(255, 220, 156)
     )
 
     coin = getcoin.Coin(name)
@@ -89,6 +35,42 @@ async def get(ctx, name):
     embed.add_field(name = 'Market Cap', value = coin.market_cap, inline = True)
     embed.add_field(name = '24H Volume', value = coin.volume, inline = True)
 
+    return embed
+
+@client.command(aliases=['Burst'])
+async def burst(ctx):
+    embed = makeEmbed('burst')
+    embed.set_author(name = 'BURST', icon_url = 'https://i.imgur.com/ipP9Bu3.png')
+
     await ctx.send(embed = embed)
+
+@client.command(aliases=['Bhd'])
+async def bhd(ctx):
+    embed = makeEmbed('bitcoinhd')
+    embed.set_author(name = 'BitcoinHD', icon_url = 'https://s2.coinmarketcap.com/static/img/coins/64x64/3966.png')
+
+    await ctx.send(embed = embed)
+
+@client.command(aliases=['Bitcoin','Btc','bitcoin'])
+async def btc(ctx):
+    embed = makeEmbed('bitcoin')
+    embed.set_author(name = 'Bitcoin', icon_url = 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png')
+
+    await ctx.send(embed = embed)
+
+@client.command()
+@commands.cooldown(3, 30, commands.BucketType.user)
+async def get(ctx, name):
+    embed = makeEmbed(name)
+
+    await ctx.send(embed = embed)
+
+@get.error
+async def get_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        msg = 'Calm down, I don\'t want to be rate limited, please try again in {:.0f}s'.format(error.retry_after)
+        await ctx.send(msg)
+    else:
+        raise error
 
 client.run(TOKEN)    
